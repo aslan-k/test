ad3cbc970dc465583ee5cb8243368f69b394b5be
Aslan <kerimov_aslan@mail.ru>
2019-03-09 21:25:59 +0300

    коммит на первое ДЗ

--- Урок №1 (Введение в JS, типы данных и операторы)/HomeWork_obligatory/script.js
+++ Урок №1 (Введение в JS, типы данных и операторы)/HomeWork_obligatory/script.js
@@ -1,54 +1,28 @@
-
 "use strict";
 
 var money = prompt("Ваш бюджет на месяц?", "");
-console.log(money)
+console.log(money);
 
 var time = prompt("Введите дату в формате YYYY-MM-DD", "");
-console.log(time)
-
-
-var appData = {
-    budget: money,
-    timeData: time,
-    isMarried: false
-}
+console.log(time);
 
 var firstQuestion = prompt("Введите обязательную статью расходов в этом месяце", "");
 console.log(firstQuestion);
 var secondQuestion = prompt("Во сколько обойдется?", "");
 console.log(secondQuestion);
 
-expenses: {
-    firstQuestion : secondQuestion
-    }
-
-
-/*
-3) Создать объект appData, который будет содержать такие данные:
-
-·      бюджет (передаем сюда переменную из п.2)
-
-·      данные времени - timeData (передаем сюда переменную из п.2)
-
-·      объект с обязательными расходами - expenses (смотри пункт 4)
-
-·      объект с необязательными расходами - optionalExpenses (оставляем пока пустым)
-
-·      массив данных с доп. доходом - income (оставляем пока пустым)
-
-·      свойство savings (выставляем его как false)
-4) Задать пользователю по 2 раза вопросы:
-
-    “Введите обязательную статью расходов в этом месяце”
-
-    “Во сколько обойдется?”
-
-    Записать ответы в объект expenses в формате: 
-
+var appData = {
+    budget: money,
+    timeData: time,
     expenses: {
+        firstQuestion : secondQuestion
+        },
+    optionalExpenses: {},
+    income: [],
+    saving: false
+};
+console.log("Бюджет в один день:" + appData.budget/30);
 
-    “ответ на первый вопрос” : “ответ на второй вопрос”
 
-    }
-*/
+    
+
---
+++ Урок №1 (Введение в JS, типы данных и операторы)/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+//переменные, типы данных:
+var number = 5;
+var string = "hello";
+var sym = Symbol(); //редко исользуется 
+var boolean = true;
+null;//вывод если ссылка на несуществующую вещь
+undefined;//вывод переменной с не присвоенным значением
+
+//var obj = {};
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.name);// вывод свойства объекта
+
+//другой способ вывода свойства объекта
+console.log(persone["name"]);
+
+let arr = ["plum.png", "orange.png", "apple.bmp"];//massive 
+console.log(arr[2]);
+
+//-----------общение с клиентом................
+//alert("я тут") //сообщение в строенном модельном окне
+let answer;
+//answer = confirm("вам 18?");//да-нет вопрос пользователю
+answer = prompt("вам 18?", "");//специальный вопрос пользователю
+console.log(answer)
+
+console.log(typeof(arr))//выводит тип данного arr 
+console.log(typeof(null))//типа ошибка ...
+
+//---------------ОПЕРАТОРЫ ............................
