e23cffeeb48a82532f5deeb8cdac78d2a8ed6163
Aslan <kerimov_aslan@mail.ru>
2019-03-12 19:05:26 +0300

    commit for Lesson_01 and Lesson_02

--- Lesson_01/script.js
+++ Lesson_01/script.js
@@ -16,11 +16,9 @@ let a1 = prompt("Введите обязательную статью расхо
     a2 = prompt("Во сколько обойдется?", ""),
     a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
     a4 = prompt("Во сколько обойдется?", "");
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-//appData.expenses["a1"] = "a2";
-//appData.expenses["a3"] = "a4";
+    
+appData.expenses["a1"] = "a2";
+appData.expenses["a3"] = "a4";
 
 console.log("Бюджет за 1 день:" + appData.budget/30);
 
