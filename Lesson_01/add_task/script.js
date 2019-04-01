96afe2be39d54c1fa00bb86d5e5052f12ba39c4d
Aslan <kerimov_aslan@mail.ru>
2019-04-01 21:22:11 +0300

    Commit for add_Task 01_02

--- Lesson_01/add_task/script.js
+++ Lesson_01/add_task/script.js
@@ -1,7 +1,7 @@
 let num = 33721,
     multiple = 1,
     exp = 1;
-let getNum = (n1) => {return n1.toString().split('');}
+let getNum = n1 => { return n1.toString().split(''); }
 let arr = getNum(num);
 
 for(let i = 0; i < arr.length; i++) { multiple = multiple*arr[i]; }
@@ -10,7 +10,7 @@ console.log(`Произведение цифр: ${multiple}`);
 for(let i = 0; i < 3; i++) { exp = exp*multiple; }
 console.log(`В 3 степени: ${exp}`);
 
-let getTwoNum = (n2) => {return n2.toString().split('', 2);} 
+let getTwoNum = n2 => { return n2.toString().split('', 2); } 
 console.log(`Первые 2 цифры: ${getTwoNum(exp)}`);
  
 
--- Lesson_02/add_task/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
-
-document.write(`<p> ${week[0].italics()} </p>`);
-for(let i = 1; i < week.length; i++) {
-    (i == week.length-1 || i == week.length-2) ? document.write(`<p> ${week[i].bold()} </p>`) : 
-    document.write(`<p> ${week[i]} </p>`);
-}
